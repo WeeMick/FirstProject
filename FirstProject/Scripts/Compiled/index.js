900ce3b72c21a0139a606ddac337cfdb16f8957f
WeeMick <michaelblr5@gmail.com>
2024-03-14 15:47:34 +0000

    commits before moving to vscode

--- FirstProject/Scripts/Compiled/index.js
+++ FirstProject/Scripts/Compiled/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hello");
+var button = document.getElementById("button1");
+if (button) {
+    button.addEventListener('click', loadText);
+}
+function loadText() {
+    var xhr = new XMLHttpRequest();
+    //console.log(xhr);
+    xhr.open('GET', 'Models/sample.txt', true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            console.log(this.responseText);
+        }
+        else {
+            console.log("Something went wrong: ");
+        }
+    };
+    xhr.send();
+}
--- FirstProject/Scripts/index.ts
+++ FirstProject/Scripts/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello");
+const button = document.getElementById("button1");
+
+if (button) {
+    button.addEventListener('click', loadText);
+}
+
+function loadText() {
+    const xhr = new XMLHttpRequest();
+    //console.log(xhr);
+
+    xhr.open('GET', 'Models/sample.txt', true);
+
+    xhr.onload = function() {
+        if (this.status == 200) {
+            console.log(this.responseText);
+        }
+        else {
+            console.log("Something went wrong: ");
+        }
+    }
+
+    xhr.send();
+}
